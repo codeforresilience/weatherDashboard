e0122a21405d0c8f85f3238f6b9a4120eaecc3c5
Amos Nzaga <nzagajunior@gmail.com>
2016-06-02 01:45:22 +0300

    add icons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,11 +14,30 @@ angular.module('tandaleApp')
       'AngularJS',
       'Karma'
     ];
+
 		function getTime(){
 	    	$interval(function(){
+	    		$scope.days = [];
 				$scope.time = moment().format('LTS');
 	      		$scope.date = moment().format('ll');
 	      		$scope.day = moment().format('dddd');
+	      		$scope.day1 = moment().subtract(1, 'days');
+	      		$scope.day2 = moment($scope.day1).subtract(1, 'days');
+	      		$scope.day3 = moment($scope.day2).subtract(1, 'days');
+	      		$scope.day4 = moment($scope.day3).subtract(1, 'days');
+	      		$scope.day5 = moment($scope.day4).subtract(1, 'days');
+	      		$scope.day6 = moment($scope.day5).subtract(1, 'days');
+	      		$scope.day7 = moment($scope.day6).subtract(1, 'days');
+	      		$scope.days.push({day: $scope.day1.format('dddd'), date: $scope.day1.format('ll') });
+	      		$scope.days.push({day: $scope.day2.format('dddd'), date: $scope.day2.format('ll') });
+	      		$scope.days.push({day: $scope.day3.format('dddd'), date: $scope.day3.format('ll') });
+	      		$scope.days.push({day: $scope.day4.format('dddd'), date: $scope.day4.format('ll') });
+	      		$scope.days.push({day: $scope.day5.format('dddd'), date: $scope.day5.format('ll') });
+	      		$scope.days.push({day: $scope.day6.format('dddd'), date: $scope.day6.format('ll') });
+	      		$scope.days.push({day: $scope.day7.format('dddd'), date: $scope.day7.format('ll') });
+	      		
+	      		
+
 				},1000);
 		    }
 
