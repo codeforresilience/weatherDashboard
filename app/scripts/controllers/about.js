c2a3331afda52152e75a43ce0e796e63368cd3f7
Amos Nzaga <nzagajunior@gmail.com>
2016-06-01 22:13:57 +0300

    Initialize repo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tandaleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tandaleApp
+ */
+angular.module('tandaleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tandaleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tandaleApp
+ */
+angular.module('tandaleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
